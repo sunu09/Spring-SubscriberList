606bcb0cf78cbd19db6642468e6504eb452a52a1
sunu09 <74618256+sunu09@users.noreply.github.com>
2021-02-08 22:50:25 -0600

    first commit added photo

--- src/main/java/com/tts/subscriberlist/subscriber/SubscriberRepository.java
+++ src/main/java/com/tts/subscriberlist/subscriber/SubscriberRepository.java
@@ -5,9 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface SubscriberRepository extends CrudRepository<Subscriber, Long> {
-//    List<Subscriber> search(String keyword);
-    /* -----from Josh
-        @Query("SELECT s FROM Subscriber s WHERE s.firstName LIKE %?1% OR s.lastName LIKE %?1% OR s.userName LIKE %?1% OR s.id LIKE %?1% ")
-    public List<Subscriber> search(String keyword);
-     */
+
 }
